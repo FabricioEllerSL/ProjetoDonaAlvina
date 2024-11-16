111124649445094381dbc1ed4ad5ab5795a106f5
FabricioEllerSL <fabricioellersl@gmail.com>
2024-11-16 15:28:16 -0300

    :sparkles: feat: add navigation page

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "home"
+
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),
     path('navegacao/', views.navigation_page, name='navigation_page'),
