91a130263c6b3d5bd841fd264d9dfc64db6f650b
FabricioEllerSL <fabricioellersl@gmail.com>
2024-11-16 16:33:47 -0300

    :sparkles: feat: adding str method and new fields

--- vendas/models.py
+++ vendas/models.py
@@ -3,4 +3,11 @@ from django.db import models
 class Venda(models.Model):
     nome_cliente = models.CharField(max_length=50)
     valor_un_kg = models.IntegerField()
-    qtd_kgs = models.DecimalField(max_digits=8, decimal_places=1)
+    qtd_kgs = models.DecimalField(max_digits=8, decimal_places=2)
+    data_venda = models.DateTimeField(auto_now_add=True)
+    descricao_venda = models.TextField(default="")
+
+    def __str__(self):
+        valor_total = self.valor_un_kg * self.qtd_kgs
+        data_formatada = self.data_venda.strftime('%d/%m/%Y')
+        return f'Venda | Cliente: {self.nome_cliente}, Data: {data_formatada} Valor: R${str(valor_total).replace('.', ',')}'
