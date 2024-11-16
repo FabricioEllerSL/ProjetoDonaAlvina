35a74397df599cad6194745a434efc9d267da286
FabricioEllerSL <fabricioellersl@gmail.com>
2024-11-16 15:53:16 -0300

    :sparkles: feat: creating model Venda

--- vendas/admin.py
+++ vendas/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Venda
 
-# Register your models here.
+@admin.register(Venda)
+class VendaAdmin(admin.ModelAdmin):
+    list_display = ('nome_cliente', 'valor_un_kg', 'qtd_kgs')
--- vendas/models.py
+++ vendas/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Venda(models.Model):
+    nome_cliente = models.CharField(max_length=50)
+    valor_un_kg = models.IntegerField()
+    qtd_kgs = models.DecimalField(max_digits=8, decimal_places=1)
