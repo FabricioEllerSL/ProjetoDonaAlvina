3f39415bee3bd7610689671b3465f03003ae5a37
FabricioEllerSL <fabricioellersl@gmail.com>
2024-12-27 08:52:52 -0300

    :sparkles: feat: new css for register page

--- vendas/forms.py
+++ vendas/forms.py
@@ -6,5 +6,18 @@ class VendaForm(forms.ModelForm):
         model = Venda
         fields = ['nome_cliente', 'valor_un_kg', 'qtd_kgs', 'descricao_venda']  # Campos que você quer exibir no formulário
         widgets = {
-            'descricao_venda': forms.Textarea(attrs={'rows': 4, 'cols': 40}),  # Definindo o tamanho do campo de texto
+            'descricao_venda': forms.Textarea(attrs={
+                'rows': 4, 
+                'cols': 40,
+                'class': 'textarea-field',
+                }),
+            'nome_cliente': forms.TextInput(attrs={
+                'class': 'input-field'
+            }),
+            'valor_un_kg': forms.TextInput(attrs={
+                'class': 'input-field'
+            }),
+            'qtd_kgs': forms.TextInput(attrs={
+                'class': 'input-field'
+            })
         }
