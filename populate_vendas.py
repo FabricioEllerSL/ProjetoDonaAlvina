f871efcb1c45909c40287473f8d9a809a68d1d5d
FabricioEllerSL <fabricioellersl@gmail.com>
2024-12-28 10:55:29 -0300

    :sparkles: feat: added payment method in vendas generator

--- populate_vendas.py
+++ populate_vendas.py
@@ -4,32 +4,37 @@ from random import randint, uniform
 from decimal import Decimal
 from faker import Faker
 
-# Configurar o ambiente Django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'linguica_divina.settings')  # Substitua 'nome_do_projeto' pelo nome do seu projeto
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'linguica_divina.settings')
 django.setup()
 
-from vendas.models import Venda  # Substitua 'app' pelo nome do seu app Django
+from vendas.models import Venda
 
-# Criar instâncias de dados fictícios
-fake = Faker('pt_BR')  # Gerador de dados fictícios em português
+fake = Faker('pt_BR')
 
 def populate_vendas(qtd):
+    counter = 0
+    is_pix = 'Dinheiro'
     for _ in range(qtd):
         nome_cliente = fake.name() if randint(0, 1) else None
-        valor_un_kg = randint(10, 100)  # Valor por kg entre R$10 e R$100
-        qtd_kgs = Decimal(f"{uniform(1, 50):.2f}")  # Quantidade entre 1 e 50 kg
+        valor_un_kg = randint(10, 100)
+        qtd_kgs = Decimal(f"{uniform(1, 50):.2f}")
         descricao_venda = fake.sentence() if randint(0, 1) else None
+        metodo_pagamento = is_pix
+
+        is_pix = 'Pix' if is_pix != "Pix" else 'Dinheiro'
+
 
         Venda.objects.create(
             nome_cliente=nome_cliente,
             valor_un_kg=valor_un_kg,
             qtd_kgs=qtd_kgs,
-            descricao_venda=descricao_venda
+            descricao_venda=descricao_venda,
+            metodo_pagamento=metodo_pagamento
         )
 
     print(f'{qtd} vendas foram adicionadas ao banco de dados.')
 
-# Chamar a função para popular o banco
+
 if __name__ == '__main__':
-    quantidade = 10  # Defina o número de vendas a serem criadas
+    quantidade = 10
     populate_vendas(quantidade)
