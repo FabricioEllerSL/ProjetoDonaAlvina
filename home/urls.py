0a1deb91d2428897190698e9fbb814f598c73e53
FabricioEllerSL <fabricioellersl@gmail.com>
2024-12-31 12:12:45 -0300

    :sparkles: feat: simple login page

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,6 @@ app_name = "home"
 
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),
+    path('login/', views.login_page, name='login_page'),
     path('navegacao/', views.navigation_page, name='navigation_page'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
 from datetime import datetime
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from .forms import LoginForm
 
 def landing_page(request):
 
@@ -27,4 +30,25 @@ def navigation_page(request):
     }
 
     return render(request, 'home/navigation.html', context_)
+
+# LOGIN SECTION
+
+def login_page(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'Login realizado com sucesso!')
+                return redirect('home:navigation_page')  # Substitua 'home' pela URL desejada
+            else:
+                messages.error(request, 'Usuário ou senha inválidos.')
+    else:
+        form = LoginForm()
+
+    return render(request, 'home/login.html', {'form': form})
+
 
